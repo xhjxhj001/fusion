3fc5a486ff38239462dd6b9156d6eaa566324784
xhjxhj001 <xhjxhj001@gmail.com>
2018-12-17 19:06:18 +0800

    update script

--- config/restart.sh
+++ config/restart.sh
@@ -1,7 +1,16 @@
 #!bin/bash
+############## check if there is an indicated server name ###########
+ARG=
+if [ $1 -a $1 != '--composer' ]; then
+	ARG=$1		
+fi
+############## restart docker container ###################
 docker-compose  down
-docker-compose  up -d
-if [ $1 -a $1 = '--composer' ]; then
+docker-compose  up -d $ARG
+###########################################################
+
+############## check if should update composer ############
+if [[ $1 && $1 == '--composer' ]] || [[ $2 && $2 == '--composer' ]]; then
 	cd ../backend
 	composer update
 	cd -
